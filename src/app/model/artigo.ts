41a52297c51f4267ca7e6c20961590e54432680b
HUGO HIKARI DE SIQUEIRA <hugo.hsiqueira@senacsp.edu.br>
2020-03-06 20:35:04 -0300

    my first commit

---
+++ src/app/model/artigo.ts
@@ -0,0 +1,10 @@
+export class Artigo{
+    constructor (
+        public id:number,
+        public titulo:string,
+        public autor:string,
+        public data:string,
+        public imgUrl:string,
+        public texto:string){}
+        
+}
